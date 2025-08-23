d71bda09bf0eabc46ebc4d01e3035b8b5a5c40b9
Jefferson Barros <jeffersonbds@gmail.com>
2025-08-23 14:18:40 -0300

    Api para configurações de variaveis de ambiente dos sistemas web

---
+++ ConfigSystem/ConfigSystemApi/Controllers/ConfigController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigSystemApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/config/
+        [HttpGet("{system}/{client}/{key}")]
+        public IActionResult GetKeyValue(string system, string client, string key)
+        {
+            var sectionPath = $"{system}:{client}:{key}";
+            var keyValue = _configuration[sectionPath];
+
+            if (key == "Block")
+                return Ok(new { System = system, Client = client, Block = keyValue });
+            else
+                return NotFound($"Chave não encontrada para  '{system}'.");
+        }
+    }
+
+
+}
